72ab5278adf9c12232851cd4c9ebe8916eef67ca
Rafael Pires <rafael.pires@unine.ch>
2017-07-19 21:43:27 +0200

    GMP compiling with mocked libc

--- gmp-6.1.2/assert.c
+++ gmp-6.1.2/assert.c
@@ -32,8 +32,13 @@ You should have received copies of the GNU General Public License and the
 GNU Lesser General Public License along with the GNU MP Library.  If not,
 see https://www.gnu.org/licenses/.  */
 
+#ifdef COMPILE_SGX
+#include <libc_proxy.h>
+#else
 #include <stdio.h>
 #include <stdlib.h>
+#endif
+
 #include "gmp.h"
 #include "gmp-impl.h"
 
