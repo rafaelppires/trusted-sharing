9d60ea18b6f0168dd9f01a0af03d940067f2472e
Rafael Pires <rafael.pires@unine.ch>
2017-09-26 22:16:59 +0200

    SGX compilation of POC

--- sgx_poc/src/curves.cpp
+++ sgx_poc/src/curves.cpp
@@ -20,13 +20,37 @@ Equivalence to RSA:
 128		3072
 */
 
-#include <pbc.h>
+#include <stdio.h>
+
 #ifdef ENABLE_SGX
 #include <enclave_curves_u.h>
+#include <sgx_initenclave.h>
+#include <unistd.h>
+#include <libgen.h>
+sgx_enclave_id_t global_eid = 0;    /* global enclave id */
+#else
+#include <pbc.h>
 #endif
 
-int main() {
+int main( int argc, char **argv ) {
 #ifdef ENABLE_SGX
+    /* Changing dir to where the executable is.*/
+    char *ptr = realpath( dirname(argv[0]),NULL );
+
+    if( ptr == NULL ){ perror("Error:"); abort(); }
+    if( chdir(ptr) != 0) abort();
+
+    /* Initialize the enclave */
+    if(initialize_enclave( global_eid,
+                           "curves.signed.so","enclave.curvespbc.token") < 0) {
+        return -2;
+    }
+    free(ptr);
+
+    sgx_status_t ret1, ret2;
+    ret1 = ecall_handlerequest(global_eid, 224,1024);
+    ret2 = ecall_handlerequest(global_eid, 256,1536);
+#else
 	pbc_param_t par;
 	pbc_param_init_a_gen(par, 224, 1024);
 	FILE* pf = fopen ("a_224_1024.param", "w");
@@ -39,9 +63,6 @@ int main() {
 	pf1 = fopen ("a_256_1536.txt", "w") ;
 	pbc_param_out_str(pf1, par1);
 	fclose(pf1);
-#else
-    ecall_handlerequest(224,1024);
-    ecall_handlerequest(256,1536);
 #endif
 }
 
--- sgx_poc/src/ecalls_curves.cpp
+++ sgx_poc/src/ecalls_curves.cpp
@@ -1,6 +1,21 @@
+#include <pbc.h>
 #include <enclave_curves_t.h>
 
-void ecall_handlerequest( int a, int b ) {
+extern "C" {
+void printf(const char *fmt, ...) {
+    char buf[BUFSIZ] = {'\0'};
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(buf, BUFSIZ, fmt, ap);
+    va_end(ap);
+    ocall_print(buf);
+}
+}
 
+//====================== ECALLS ================================================
+void ecall_handlerequest( int a, int b ) {
+    ocall_print("hallo");
+    pbc_param_t par;
+    pbc_param_init_a_gen(par, 224, 1024);
 }
 
